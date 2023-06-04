32b6c1d4a31a1e3982d9a46315090a9f5d860699
Jose Lo <joalloce@gmail.com>
2023-06-04 12:01:54 -0600

    style(index.css): tailwind config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,9 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1> Hello World</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
